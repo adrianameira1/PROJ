e609fa02f9a2576ec8bd9ad3e2bc637a8cf43a8b
adrianameira <adriana.meira@ipvc.pt>
2025-04-25 22:39:40 +0100

    Adicao do backoffice para aplicação DESKTOP

---
+++ backoffice/src/main/java/com/backoffice/MainApp.java
@@ -0,0 +1,23 @@
+package com.backoffice;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Carrega o FXML que vamos criar no próximo passo:
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainView.fxml"));
+        primaryStage.setTitle("BackOffice – Quinta Eventos");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
